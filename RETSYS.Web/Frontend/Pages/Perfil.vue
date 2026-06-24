3c194c535571b338c418553c6076df799775e79d
Rafael V. Wierzba <rafaelwierzba@gmail.com>
2026-06-24 12:11:52 -0300

    fix: corrigido import do ref de react para vue no perfil

--- RETSYS.Web/Frontend/Pages/Perfil.vue
+++ RETSYS.Web/Frontend/Pages/Perfil.vue
@@ -86,7 +86,7 @@
 </template>
 
 <script setup>
-import { ref } from 'react'
+import { ref } from 'vue'
 import { useForm } from '@inertiajs/vue3'
 import AuthenticatedLayout from '../Shared/AuthenticatedLayout.vue'
 
